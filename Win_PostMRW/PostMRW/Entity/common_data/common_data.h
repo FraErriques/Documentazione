4ad2793ea111c87237c94b2fef92a59c94e14240
FraErriques <francesco.erriques@gmail.com>
2021-04-08 21:37:58 +0200

    using Repo Documentazione:: to keep track of the changes of PostMWR on Win platform. On Win this project has diverged significantly from the Linux version, the main one.

---
+++ Win_PostMRW/PostMRW/Entity/common_data/common_data.h
@@ -0,0 +1,36 @@
+
+# ifndef _COMMON_DATA_
+# define _COMMON_DATA_
+
+#include <cstddef>
+#include <cmath>
+
+typedef short Domain; // type for throwing exceptions
+
+const double M_PI = atan(+1.0) * +4.0;//  needed only on Windows ,while on Linux it's a symbolic constant.
+
+#define PI M_PI // once I called it that, and now I don't want to alter the whole source.
+//const double PI = 3.1415926535897932384626433832795; // double PI = 6.0 * arctan ( pow (3.0, -0.5) );
+//const double E  = 2.7182818284590452353602874713527;
+
+#include <iostream>
+struct Crash // a type for Exception-Handling
+{
+   Crash (const char * message) {std::cout << "\n\t" << message << "\n\t";}
+   Crash (void)                 {char  message[] = "\n\tMemory Fault\n\n"; std::cout<<message;}
+};
+
+struct Couple // the generic vector of R2, used by the interpolation methods
+{
+   double argument; // abscissa
+   double image;    // ordinate
+};
+/*
+# ifdef _USE_VECTOR_
+   # include "vector.h"
+   typedef Vector < Couple > Table; // Class for a vector of R2 points
+# endif
+*/
+
+# endif
+
