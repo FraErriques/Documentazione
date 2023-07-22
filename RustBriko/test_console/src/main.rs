fa589e488e8077fc957acce6a2a8e96e606208aa
FraErriques <francesco.erriques@gmail.com>
2023-07-22 23:04:32 +0200

    Boxing in Rust

--- RustBriko/test_console/src/main.rs
+++ RustBriko/test_console/src/main.rs
@@ -12,6 +12,10 @@ fn vector_builder(){
     let _xs = vec![1i32, 2, 3];
 }
 
+fn tuple_returner() -> (i32, f64){
+    return (8, 2.7182818);
+}
+
 /* this is the entry point of Documentazione::RustBriko ,i.e. Documentazione::RustBriko::test_console 
    the syntax for doing that, is: 
     extern crate common;
@@ -31,4 +35,8 @@ fn main() {
     some_entity_layout_::some_entity_layout_signature();
     array_builder();
     vector_builder();
+    let tu = tuple_returner();
+    println!(" {}  {}", tu.0, tu.1 ); 
+    let din_tu = Box::new(tu);
+    println!(" {}  {}", din_tu.0, din_tu.1 ); 
 }// main
