c35b3d2c1c65c99e61477a97139c017161382a66
FraErriques <francesco.erriques@gmail.com>
2023-12-22 13:02:42 +0100

    a test session held on ITBZOW1069

--- Rust_table/src/main.rs
+++ Rust_table/src/main.rs
@@ -1,6 +1,9 @@
 
 
 fn main() {
+    /*********************************************************
+    last test held on machine ITBZOW1069 on 2023.December.22
+     ********************************************************/
 
     // this is a typedef of a row-layout of a  db-table. First wrote on July2023 in Marina di Grosseto.
     /* the syntax is :
