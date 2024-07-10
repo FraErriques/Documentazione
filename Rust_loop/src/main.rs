abf655c5512e37111d4ba6fdb295069fecedfb33
FraErriques <francesco.erriques@gmail.com>
2024-07-10 15:42:53 +0200

    test on ITBZOW1069

---
+++ Rust_loop/src/main.rs
@@ -0,0 +1,32 @@
+
+
+fn main() {
+    let value = 456;
+    let mut x = 1;
+    let y = loop {
+        x *= 10;
+        if x > value {
+            break x / 10;
+        }
+    };
+    println!("largest power of ten that is smaller than value: {y}");
+
+    let mut up = 1;
+    'outer: loop {
+        let mut down = 120;
+        loop {
+            if up > 100 {
+                break 'outer;
+            }
+
+            if down < 4 {
+                break;
+            }
+
+            down /= 2;
+            up += 1;
+            println!("up: {up}, down: {down}");
+        }
+        up *= 2;
+    }
+}
---
+++ Rust_loop/target/debug/deps/rloop-15b025d66e210a12.d
@@ -0,0 +1,5 @@
+C:\root\src\GitHubSandBox\Documentazione\Rust_loop\target\debug\deps\rloop-15b025d66e210a12.exe: src/main.rs
+
+C:\root\src\GitHubSandBox\Documentazione\Rust_loop\target\debug\deps\rloop-15b025d66e210a12.d: src/main.rs
+
+src/main.rs:
