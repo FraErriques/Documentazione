c4874641ec7af4249ff4157f2468d417ab4e4bc2
FraErriques <francesco.erriques@gmail.com>
2024-07-20 22:57:58 +0200

    from Marina di Grosseto 2024.luglio.20

--- Rust_Dynamics/src/main.rs
+++ Rust_Dynamics/src/main.rs
@@ -1,10 +1,10 @@
 
 
 /// .
-fn position  (timestamp : f32) -> f32 {
-    return 9.1+timestamp;// TODO
+fn position  (timestamp : f32) -> (f32,f32,f32) {
+    return (0.0, 0.0, 9.1+timestamp);// is the motion of a falling object along the z coordinate. They are constant x=y=0.
 }// position
 
 fn main() {
-    println!("Hello, world! position is {}", position(10.0));
+    println!("Hello, world! position is {:?} ", position(10.0));
 }
--- Rust_Dynamics/target/debug/deps/Rust_Dynamics-b716557ed19e79d1.d
+++
@@ -1,5 +0,0 @@
-C:\root\src\GitHubSandBox\Documentazione\Rust_Dynamics\target\debug\deps\libRust_Dynamics-b716557ed19e79d1.rmeta: src/main.rs
-
-C:\root\src\GitHubSandBox\Documentazione\Rust_Dynamics\target\debug\deps\Rust_Dynamics-b716557ed19e79d1.d: src/main.rs
-
-src/main.rs:
