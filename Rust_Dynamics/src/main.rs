bd63a5a6c6ca02dfd6c4353c00bd3daabdf82a90
FraErriques <francesco.erriques@gmail.com>
2024-07-20 15:22:45 +0200

    from MarinaDiGrosseto Rust_Dynamics.

---
+++ Rust_Dynamics/src/main.rs
@@ -0,0 +1,10 @@
+
+
+/// .
+fn position  (timestamp : f32) -> f32 {
+    return 9.1+timestamp;// TODO
+}// position
+
+fn main() {
+    println!("Hello, world! position is {}", position(10.0));
+}
---
+++ Rust_Dynamics/target/debug/deps/Rust_Dynamics-013cdea49d95d35d.d
@@ -0,0 +1,5 @@
+C:\root\src\GitHubSandBox\Documentazione\Rust_Dynamics\target\debug\deps\libRust_Dynamics-013cdea49d95d35d.rmeta: src/main.rs
+
+C:\root\src\GitHubSandBox\Documentazione\Rust_Dynamics\target\debug\deps\Rust_Dynamics-013cdea49d95d35d.d: src/main.rs
+
+src/main.rs:
