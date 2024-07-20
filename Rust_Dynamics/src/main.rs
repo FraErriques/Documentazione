592b2d2be414eb8bd24ece0361992230253ff942
FraErriques <francesco.erriques@gmail.com>
2024-07-20 23:49:58 +0200

    from Marina di Grosseto 2024.luglio.20

--- Rust_Dynamics/src/main.rs
+++ Rust_Dynamics/src/main.rs
@@ -1,10 +1,33 @@
 
+fn velocity  (time_from : f32, time_to : f32, want_average : bool) -> (f32,f32,f32) {
+    if( want_average){
+        if(time_to-time_from <=0.0 ){
+            std::process::exit(1);
+        }
+        else {
+            return (0.0, 0.0, (position(time_to).2 -position(time_from).2)/(time_to-time_from) );
+        }
+    }
+    else{
+        if(time_to-time_from <=0.0 ){
+            std::process::exit(1);
+        }
+        else {
+            return (0.0, 0.0, (position(time_to).2-position(time_to-0.01).2)/(0.01) ) ;
+        }
+    }
+}// position
 
 /// .
-fn position  (timestamp : f32) -> (f32,f32,f32) {
-    return (0.0, 0.0, 9.1+timestamp);// is the motion of a falling object along the z coordinate. They are constant x=y=0.
+fn position  (elapsed : f32) -> (f32,f32,f32) {
+    let height = 100.0;
+    return (0.0, 0.0, height-9.81*elapsed);// is the motion of a falling object along the z coordinate. They are constant x=y=0.
 }// position
 
 fn main() {
-    println!("Hello, world! position is {:?} ", position(10.0));
+    let elapsed = 6.1;
+    println!("This is the motion of a falling object along the z coordinate. They are constant x=y=0. position is {:?} ",
+      position(elapsed));
+      println!("The AVERAGE velocity is  {:?} ",  velocity(0.0, elapsed, true) );  
+      println!("The POINT velocity is  {:?} "  ,  velocity(0.0, elapsed, false) );  
 }
