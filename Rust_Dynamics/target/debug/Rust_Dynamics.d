5da9e368d5f580b61c92ddd6e7adcd9cde110f61
FraErriques <francesco.erriques@gmail.com>
2024-08-02 15:19:07 +0200

    test on ITBZOW1069

--- Rust_Dynamics/target/debug/Rust_Dynamics.d
+++ Rust_Dynamics/target/debug/Rust_Dynamics.d
@@ -1,1 +1,1 @@
-D:\root\src\GitHubSandBox\Documentazione\Rust_Dynamics\target\debug\Rust_Dynamics.exe: D:\root\src\GitHubSandBox\Documentazione\Rust_Dynamics\src\main.rs
+C:\root\src\GitHubSandBox\Documentazione\Rust_Dynamics\target\debug\Rust_Dynamics.exe: C:\root\src\GitHubSandBox\Documentazione\Rust_Dynamics\src\main.rs
