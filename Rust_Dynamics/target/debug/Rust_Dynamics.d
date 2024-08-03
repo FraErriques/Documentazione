20c6e92acee4d10c4a81d1d81d199ea6f31e814e
FraErriques <francesco.erriques@gmail.com>
2024-08-03 19:40:38 +0200

    from Huygens::Obz

--- Rust_Dynamics/target/debug/Rust_Dynamics.d
+++ Rust_Dynamics/target/debug/Rust_Dynamics.d
@@ -1,1 +1,1 @@
-C:\root\src\GitHubSandBox\Documentazione\Rust_Dynamics\target\debug\Rust_Dynamics.exe: C:\root\src\GitHubSandBox\Documentazione\Rust_Dynamics\src\main.rs
+/home/fra/src/GitHubSandBox/Documentazione/Rust_Dynamics/target/debug/Rust_Dynamics: /home/fra/src/GitHubSandBox/Documentazione/Rust_Dynamics/src/main.rs
