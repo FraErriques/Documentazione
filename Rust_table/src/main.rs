396d86b536afcbbcc3bdee6d44630e28cca491a2
FraErriques <francesco.erriques@gmail.com>
2025-04-15 19:28:33 +0200

    from Obz::Hamilton

--- Rust_table/src/main.rs
+++ Rust_table/src/main.rs
@@ -28,7 +28,8 @@
     last test held on machine ITBZOW1069 on 2025.January.22  on cargo 1.84.0 (66221abde 2024-11-19) and VSCode 1.96.4
     last test held on machine ITFORS1011 on 2025.February.07  on cargo 1.84.1 (66221abde 2024-11-19) and VSCode 1.97.0
     last test held on machine Huygens    on 2025.March.04 on     cargo 1.85.0 (d73d2caf9 2024-12-31) and VSCode 1.96.4
-    last test held on macine Weierstrass on 2025.April.05 on cargo  1.86.0 (adf9b6ad1 2025-02-28) and VSCode 1.99.0
+    last test held on macine  Weierstrass on 2025.April.05 on cargo  1.86.0 (adf9b6ad1 2025-02-28) and VSCode 1.99.0
+    last test held on macine  Hamilton::Obz on 2025.April.15 on cargo  1.86.0 (adf9b6ad1 2025-02-28) and VSCode 1.98.2
      ********************************************************/
 
 fn main() {
