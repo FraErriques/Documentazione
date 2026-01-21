c0340ba71a5f3929ade8b75d2c6b3f99446999a4
FraErriques <francesco.erriques@gmail.com>
2026-01-21 16:28:29 +0100

    from ITBZOW1069 MSYS::Mingw

---
+++ RustBriko/target/debug/deps/RustBriko-2142c3f8122129a9.d
@@ -0,0 +1,5 @@
+C:\root\src\GitHubSandBox\Documentazione\RustBriko\target\debug\deps\RustBriko-2142c3f8122129a9.d: src\main.rs
+
+C:\root\src\GitHubSandBox\Documentazione\RustBriko\target\debug\deps\libRustBriko-2142c3f8122129a9.rmeta: src\main.rs
+
+src\main.rs:
---
+++ RustBriko/target/debug/deps/RustBriko-fcb735a31ec491af.d
@@ -0,0 +1,5 @@
+C:\root\src\GitHubSandBox\Documentazione\RustBriko\target\debug\deps\RustBriko-fcb735a31ec491af.d: src\main.rs
+
+C:\root\src\GitHubSandBox\Documentazione\RustBriko\target\debug\deps\libRustBriko-fcb735a31ec491af.rmeta: src\main.rs
+
+src\main.rs:
