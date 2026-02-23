859f88b3474b17735e4727e6b38193cc5f9c29a2
FraErriques <francesco.erriques@gmail.com>
2026-02-23 17:33:59 +0100

    from ITBZOW1069 ::Documentazione.

---
+++ RustBriko/target/debug/deps/RustBriko-40f7fb0b8197f5c1.d
@@ -0,0 +1,5 @@
+C:\root\src\GitHubSandBox\Documentazione\RustBriko\target\debug\deps\RustBriko-40f7fb0b8197f5c1.d: src\main.rs
+
+C:\root\src\GitHubSandBox\Documentazione\RustBriko\target\debug\deps\libRustBriko-40f7fb0b8197f5c1.rmeta: src\main.rs
+
+src\main.rs:
---
+++ RustBriko/target/debug/deps/RustBriko-e5e3b86e1e82928f.d
@@ -0,0 +1,5 @@
+C:\root\src\GitHubSandBox\Documentazione\RustBriko\target\debug\deps\RustBriko-e5e3b86e1e82928f.d: src\main.rs
+
+C:\root\src\GitHubSandBox\Documentazione\RustBriko\target\debug\deps\libRustBriko-e5e3b86e1e82928f.rmeta: src\main.rs
+
+src\main.rs:
