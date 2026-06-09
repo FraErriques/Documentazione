e3393f08de52bba852e8faa2ffe2be56c6b2f717
FraErriques <francesco.erriques@gmail.com>
2026-06-09 15:58:34 +0200

    Ricci Tensor.

---
+++ RustBriko/target/debug/deps/RustBriko-44e81b47bdf66867.d
@@ -0,0 +1,5 @@
+C:\Users\itfraerr\root\src\GitHubSandBox\Documentazione\RustBriko\target\debug\deps\RustBriko-44e81b47bdf66867.d: src\main.rs
+
+C:\Users\itfraerr\root\src\GitHubSandBox\Documentazione\RustBriko\target\debug\deps\libRustBriko-44e81b47bdf66867.rmeta: src\main.rs
+
+src\main.rs:
---
+++ RustBriko/target/debug/deps/RustBriko-e36cc0b7b097720c.d
@@ -0,0 +1,5 @@
+C:\Users\itfraerr\root\src\GitHubSandBox\Documentazione\RustBriko\target\debug\deps\RustBriko-e36cc0b7b097720c.d: src\main.rs
+
+C:\Users\itfraerr\root\src\GitHubSandBox\Documentazione\RustBriko\target\debug\deps\libRustBriko-e36cc0b7b097720c.rmeta: src\main.rs
+
+src\main.rs:
